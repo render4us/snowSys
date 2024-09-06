5f45ea51cf7f3d9bc60525a09d6bf7e75ad5d5b4
render4us <vitormartinsg@hotmail.com>
2024-09-06 13:51:53 -0300

    Logotipo
    Logotipo

--- resources/views/pages/auth/sign-in.blade.php
+++ resources/views/pages/auth/sign-in.blade.php
@@ -16,11 +16,13 @@ Hook::action( 'ns.after-login-fields', $afterForm );
         <div class="container mx-auto p-4 md:p-0 flex-auto items-center justify-center flex">
             <div id="sign-in-box" class="w-full md:w-3/5 lg:w-2/5 xl:w-84">
                 <div class="flex justify-center items-center py-6">
-                    @if ( ! ns()->option->get( 'ns_store_square_logo', false ) )
-                    <img class="w-32" src="{{ asset( 'svg/nexopos-variant-1.svg' ) }}" alt="NexoPOS">
-                    @else
-                    <img src="{{ ns()->option->get( 'ns_store_square_logo' ) }}" alt="NexoPOS">
-                    @endif
+                    <div class="logo py-4 flex justify-center items-center">
+                        @if ( ns()->option->get( 'ns_store_rectangle_logo' ) )
+                            <img src="{{ ns()->option->get( 'ns_store_rectangle_logo' ) }}" class="w-11/12" alt="logo"/>
+                        @else
+                            <h1 class="font-black text-transparent bg-clip-text bg-gradient-to-b from-blue-200 to-indigo-400 text-3xl">SnowSys </h1>
+                        @endif
+                    </div>
                 </div>
                 <x-session-message></x-session-message>
                 {!! $beforeForm !!}
