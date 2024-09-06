34b582f44d9e90ec03f11e933e1b4407838340b7
render4us <vitormartinsg@hotmail.com>
2024-09-06 13:29:36 -0300

    Ajuste welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,13 @@
             <div class="flex justify-center items-center py-6">
                 <img class="w-32" src="{{ asset( 'svg/nexopos-variant-1.svg' ) }}" alt="NexoPOS">
             </div>
+            <div class="logo py-4 flex justify-center items-center">
+                @if ( ns()->option->get( 'ns_store_rectangle_logo' ) )
+                    <img src="{{ ns()->option->get( 'ns_store_rectangle_logo' ) }}" class="w-11/12" alt="logo"/>
+                @else
+                    <h1 class="font-black text-transparent bg-clip-text bg-gradient-to-b from-blue-200 to-indigo-400 text-3xl">SnowSys </h1>
+                @endif
+            </div>
             <div class="ns-box rounded shadow w-full md:w-1/2 lg:w-1/3 overflow-hidden">
                 <div class="ns-box-footer flex shadow border-t">
                     <div class="flex w-1/3"><a class="link text-sm w-full py-2 text-center" href="{{ ns()->route( 'ns.dashboard.home' ) }}">{{ __( 'Dashboard' ) }}</a></div>
